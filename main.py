bf73e28a4d6912812b38828d9e8c6c8a3d503d78
arctiqjacob <jacob.mammoliti@arctiq.ca>
2019-03-22 13:05:49 -0400

    updated python script. Want to write a Go one too

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ except ImportError:
 	print ("hvac library is needed. Run pip install hvac.")
 
 hvac_client = {
-	'url': vault_url,
+	'url': os.environ['VAULT_ADDR'],
 }
 
-client = hvac.Client()
+client = hvac.Client(**hvac_client)
 
 try: 
 	github_token = os.environ['GITHUB_PAT']
@@ -19,8 +19,15 @@ try:
 except KeyError:
 	github_token = input("Vault Token: ")
 
-list_response = client.secrets.kv.v2.list_secrets(
-    path='arctiq',
-)
+assert client.is_authenticated()
 
-print (json.dumps(list_response, indent=4))
+# path = "arctiq/data/users/arctiqjacob/testing"
+
+# secret_version_response = client.read(path)
+
+# for key, value in secret_version_response['data']['data'].items():
+# 	print ("vault kv put {} {}={}".format(path, key, value))
+
+list_response = client.secrets.kv.v2.list_secrets(
+	path='arctiq/users/arctiqjacob/testing'
+)
