72257ae249d4bbbf229c584f150ada05778f2483
arctiqjacob <jacob.mammoliti@arctiq.ca>
2019-03-26 13:49:14 -0400

    first working commit!

--- main.py
+++ main.py
@@ -12,6 +12,37 @@ hvac_client = {
 
 client = hvac.Client(**hvac_client)
 
+def recurse_through_secrets(path_prefix, candidate_key):
+	candidate_values = candidate_key['data']['keys']
+
+	for candidate_value in candidate_values:
+
+		next_index = path_prefix + candidate_value
+
+		if candidate_value.endswith('/') and candidate_value != 'users/':
+
+			next_value = client.secrets.kv.v2.list_secrets(path=next_index, \
+														   mount_point='arctiq')
+
+			recurse_through_secrets(next_index, next_value)
+
+		elif candidate_value != 'users/':
+			final_dict = client.read('arctiq/data/' + next_index)['data']
+
+			print ("\nvault kv put arctiq/{}".format(next_index), end='')
+						
+			final_value = final_dict['data']
+
+			try:
+				final_value = final_value.encode("utf-8")
+			except AttributeError:
+				final_value = final_value
+
+			for f in final_value:
+				print (' "{0}={1}"'.format(f, final_value[f]), end='')
+
+			print ()
+
 try: 
 	github_token = os.environ['GITHUB_PAT']
 
@@ -21,13 +52,7 @@ except KeyError:
 
 assert client.is_authenticated()
 
-# path = "arctiq/data/users/arctiqjacob/testing"
+top_level_keys = client.secrets.kv.v2.list_secrets(path='', mount_point='arctiq')
+top_vault_prefix = ''
 
-# secret_version_response = client.read(path)
-
-# for key, value in secret_version_response['data']['data'].items():
-# 	print ("vault kv put {} {}={}".format(path, key, value))
-
-list_response = client.secrets.kv.v2.list_secrets(
-	path='arctiq/users/arctiqjacob/testing'
-)
+recurse_through_secrets(top_vault_prefix, top_level_keys)
